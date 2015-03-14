2ab5b75e0249b2f7ce94ca594172a114f5332ab8
Raza Chandiwala <rachandiwala@gmail.com>
2015-03-14 18:17:49 -0400

    Displaying Price in Dollars

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ActiveRecord::Base
 
-validates :description, :name, :url, presence: true
-validates :price, numericality: {only_integer: true}
+  validates :description, :name, :url, presence: true
+  validates :price, numericality: {only_integer: true}
 
+
+
+  def formatted_price
+    price_in_dollars = price.to_f 
+    sprintf("%.2f", price_in_dollars)
+  end
 end
