7a9a8a9bf90a4aea5cfb977fe20cb1e7362f6de7
Raza Chandiwala <rachandiwala@gmail.com>
2015-03-19 16:21:53 -0400

    Added getscript

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -9,7 +9,7 @@
 
 */
 
-/*
+
 $(document).on('ready page:load', function() {
   $('#search-form').submit(function(event) {
     event.preventDefault();
@@ -25,7 +25,7 @@ $(document).on('ready page:load', function() {
   });
 });
 
-*/
+/*
 
 // This code does the same thing as above, however with JQuery and Ajax SortHand Method
 
@@ -44,7 +44,55 @@ $(document).on('ready page:load', function() {
   });
 
 
-/*
-Using $.get doesn't require anything but the url, since the 'GET' request method and the data type we're looking for are the defaults of this function. This lets us write less code!
+
+
+Using $.get doesn't require anything but the url, since the 'GET' request method and the data 
+type we're looking for are the defaults of this function. This lets us write less code!
 
 */
+
+/*
+        $.getScript()
+
+$.getScript() is another shorthand function that you can use. Instead of using the data type 
+"string" or "html", it uses "script". This means it's expecting a "script" as a response. 
+Once it gets that response, it will execute the script. 
+That means we no longer need to have a callback, the script acts as a callback.
+
+EX:
+
+$(document).on('ready page:load', function() {
+  $('#search-form').submit(function(event) {
+    event.preventDefault();
+    var searchValue = $('#search').val();
+
+  $.getScript('/products?search=' + searchValue);
+
+--->
+ * Since $.getScript is requesting something with the data type "script", the server needs to be 
+  able to respond to that data type. In order to get that to work, our controller can use 
+  respond_to generate different responses for different request types.
+
+app/controllers/products_controller.rb
+Replace: if request.xhr?
+          render @products
+
+with:
+
+respond_to do |format|
+      format.html
+      format.js
+    end
+
+----->
+when it comes to JS responses, it looks for a view template just as it does with HTML, except it'll 
+look for index.js.erb instead of index.html.erb.
+
+j is sort for  'escape_javascript'
+
+app/views/products/index.js.erb
+Add:
+
+$('#products').append('<%= j(render @products) %>');
+$('.pagination').replaceWith('<%= j(paginate @products) %>');
+$('.page-entries').html('<%= j(page_entries_info @products) %>');
